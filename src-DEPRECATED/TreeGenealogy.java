d1e6d2d4a87b47901e22a6ae81108641cd07632e
nmigacz <nickmigacz@gmail.com>
2020-12-04 17:13:54 -0600

    search page started

---
+++ src-DEPRECATED/TreeGenealogy.java
@@ -0,0 +1,363 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Random;
+import java.util.Set;
+
+public class TreeGenealogy{
+    private static Random r = new Random();
+    private static final String defaultFamilyTree = "FamilyTreeInputTextFileV2.txt";
+    private Map<String, Relationship> relations = new HashMap<>();
+    private Map<String, Person> people          = new HashMap<>();
+    private List<String> personList = new ArrayList<>();
+    private int initialPersonCapacity = 0;
+    private int initialRelationshipCapacity = 0;
+
+    //main function meant to test loading of data
+    public static void main(String[] args) throws IOException{
+        String treeFile = (args.length == 1 ? args[0] : defaultFamilyTree);
+
+        TreeGenealogy t = new TreeGenealogy(treeFile);
+        //at this point t is now populated with all data from the family tree text file.
+
+        Scanner myScan = new Scanner(System.in);
+        int menuSelection = 0;
+
+        while(menuSelection != 8){
+            displayMainMenu();
+            try {
+                menuSelection = myScan.nextInt();
+                switch (menuSelection){
+                    case 1:
+                        t.printAllRelationships();
+                        break;
+                    case 2:
+                        t.searchRelationShip(myScan);
+                        break;
+                    case 3:
+                        t.searchPerson(myScan);
+                        break;
+                    case 4:
+                        t.searchGrandParents(myScan);
+                        break;
+                    case 5:
+                        t.searchFirstName(myScan);
+                        break;
+                    case 6:
+                        t.searchLastName(myScan);
+                        break;
+                    case 7:
+                        t.loadFile(myScan);
+                        break;
+                    case 8:
+                        //this is just an exit condition for the while loop
+                        break;
+                    default:
+                        System.out.println("Entry " + menuSelection + " is not a listed option.");
+                }
+            } catch (Exception e){
+                System.out.println("ERROR INPUT: " + myScan.next() + " is not valid.");
+            }
+            System.out.println("\n\n");
+        }
+    }
+
+    public static void displayMainMenu(){
+        /*
+         * 1. List all relationships
+         * 2. Print relationship with rID
+         * 3. Print person with pID
+         * 4. Print grandparents of pID
+         * 5. Search by first name
+         * 6. Search by last name
+         */
+        String[] menuItems = new String[8];
+        menuItems[0] = "1. List all relationships     ";
+        menuItems[1] = "2. Print relationship with rID";
+        menuItems[2] = "3. print person with pID      ";
+        menuItems[3] = "4. Print grandparents of pID  ";
+        menuItems[4] = "5. Search by first name       ";
+        menuItems[5] = "6. Search by last name        ";
+        menuItems[6] = "7. Load Data                  ";
+        menuItems[7] = "8. Exit program               ";
+
+        for(String s : Person.encapsulateWithBorder('+', menuItems))
+            System.out.println(s);
+
+    }
+
+    public void printAllRelationships(){
+        for(Map.Entry<String, Relationship> entry : relations.entrySet()){
+            entry.getValue().printVisuals();
+            System.out.println();
+        }
+    }
+
+    public void loadFile(Scanner myScan){
+        myScan.nextLine();
+        String fileName;
+        System.out.println("Enter file name to load: ");
+        try {
+            fileName = myScan.nextLine();
+            try {
+                loadData(fileName);
+            } catch (IOException ioe){
+                System.out.println("Error opening the file. File " + fileName + " may not exist.");
+            }
+        } catch (Exception e){
+            System.out.println("ERROR INPUT: " + myScan.next() + " is not valid.");
+        }
+    }
+
+    public void searchGrandParents(Scanner myScan){
+        myScan.nextLine();
+        String pEntered;
+        System.out.println("Enter pID to search for grand parents: ");
+        try{
+            pEntered = myScan.nextLine();
+            printGrandParents(pEntered);
+        } catch (Exception e){
+            System.out.println("ERROR INPUT: " + myScan.next() + " is not valid.");
+        }
+    }
+
+    public void printGrandParents(String pID){
+        if (people.containsKey(pID)) {
+            Person p = people.get(pID);
+            String r = p.getChildOfR();
+            if (r == null || (relations.get(r).getFemaleParent() == null && relations.get(r).getMaleParent() == null ))
+                System.out.println(p.fullNameString() + " does not have any parents listed, so there are no grandparents listed.\n");
+            else {
+                int grandParentsFound = 0;
+                Relationship rel = relations.get(r);
+
+                grandParentsFound += printParents(rel.getFemaleParent());
+                grandParentsFound += printParents(rel.getMaleParent());
+
+                if(grandParentsFound == 0)
+                    System.out.println("No grandparents were found.\n");
+                else
+                    System.out.println("Total grandparents found: " + String.valueOf(grandParentsFound) + "\n");
+            }
+        } else
+            System.out.println("No person with the personID: " + pID + " exists.\n");
+    }
+
+    public int printParents(Person p){
+        int count = 0;
+
+        if(p != null && p.getChildOfR() != null){
+            Relationship r = relations.get(p.getChildOfR());
+
+            if(r.getFemaleParent() != null) {
+                printPerson(r.getFemaleParent().getPersonID());
+                count++;
+            }
+
+            if(r.getMaleParent() != null) {
+                printPerson(r.getMaleParent().getPersonID());
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public void searchFirstName(Scanner myScan){
+        myScan.nextLine();
+        String fEntered;
+        System.out.println("Enter first name to search");
+        try{
+            fEntered = myScan.nextLine();
+            searchFirstNameAndPrint(fEntered);
+        } catch (Exception e){
+            System.out.println("ERROR INPUT: " + myScan.next() + " is not valid.");
+        }
+    }
+
+    private void searchFirstNameAndPrint(String firstName){
+        int count = 0;
+        for(Person p : people.values()){
+            if (p.getFirstName().equals(firstName)) {
+                System.out.println(p.generatePrintableVisual());
+                System.out.println();
+                count++;
+            }
+        }
+        if (count > 0)
+            System.out.println("Total people found: " + String.valueOf(count) + "\n");
+        else
+            System.out.println("No people found by the first name of: " + firstName + "\n");
+    }
+
+    public void searchLastName(Scanner myScan){
+        myScan.nextLine();
+        String lEntered;
+        System.out.println("Enter last name to search");
+        try{
+            lEntered = myScan.nextLine();
+            searchLastNameAndPrint(lEntered);
+        } catch (Exception e){
+            System.out.println("ERROR INPUT: " + myScan.next() + " is not valid.");
+        }
+    }
+
+    private void searchLastNameAndPrint(String lastName){
+        int count = 0;
+        for(Person p : people.values()){
+            if (p.getLastName().equals(lastName)) {
+                System.out.println(p.generatePrintableVisual());
+                System.out.println();
+                count++;
+            }
+        }
+        if (count > 0)
+            System.out.println("Total people found: " + String.valueOf(count) + "\n");
+        else
+            System.out.println("No people found by the last name of: " + lastName + "\n");
+    }
+
+    public void searchRelationShip(Scanner myScan){
+        myScan.nextLine();
+        String rEntered;
+        System.out.println("Enter rID to print: ");
+        try{
+            rEntered = myScan.nextLine();
+            printRelationship(rEntered);
+        } catch (Exception e){
+            System.out.println("ERROR INPUT: " + myScan.next() + " is not valid.");
+        }
+    }
+
+    public void printRelationship(String rID){
+        if (rID != null && relations.containsKey(rID)) {
+            relations.get(rID).printVisuals();
+        } else {
+            System.out.println("Relation ID: " + String.valueOf(rID) + " does not exist.");
+        }
+    }
+
+    public void printPersonParentalRelationship(String pID){
+        relations.get(people.get(pID).getChildOfR()).printVisuals();
+        System.out.println();
+    }
+
+    public void searchPerson(Scanner myScan){
+        myScan.nextLine();
+        String pEntered;
+        System.out.println("Enter pID to print: ");
+        try{
+            pEntered = myScan.nextLine();
+            printPerson(pEntered);
+        } catch (Exception e){
+            System.out.println("ERROR INPUT: " + myScan.next() + " is not valid.");
+        }
+    }
+
+    public void printPerson(String pID){
+        if (pID != null && people.containsKey(pID)) {
+            System.out.println(people.get(pID).generatePrintableVisual());
+        } else {
+            System.out.println("Person ID: " + String.valueOf(pID) + " does not exist.");
+        }
+
+    }
+
+
+    private int getOverallMaxWidth(){
+        int maxValFound = 0;
+        for( Person p : people.values())
+            maxValFound = Math.max(maxValFound, p.getMaxWidth());
+        return maxValFound;
+    }
+
+
+    /*
+    ----------------------------------------------------------
+    ----------------------------------------------------------
+    ----------------------------------------------------------
+     */
+
+    public TreeGenealogy(String dataFileName) throws IOException {
+        loadData(dataFileName);
+    }
+
+    public void loadData(String dataFileName) throws IOException {
+        List<String> data = readFile(dataFileName);
+
+
+        relations.clear();
+        people.clear();
+
+
+        //Loading Persons
+        initialPersonCapacity = Integer.parseInt((data.get(0).split(","))[1]);
+        int i = 1;
+        while( !((data.get(i).substring(0,7)).equals(",,,,,,,"))){
+            String[] d = scrapeData(data.get(i));
+            personList.add(d[0]);
+            Person p = new Person(d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7]);
+            people.put(d[0], p);
+            i++;
+        }
+        //all Persons are now loaded
+
+        i++; //skip the empty row between sections
+
+        //Loading Relationships
+        initialRelationshipCapacity = Integer.parseInt((data.get(i++).split(","))[1]);
+        while( !(data.get(i).substring(0,7).equals(",,,,,,,"))) {
+            String[] d = scrapeData(data.get(i));
+
+            Person p1 = people.get(d[1]);
+            Person p2 = people.get(d[2]);
+            if(p1 != null)
+                p1.addRelation(d[0]);
+            if(p2 != null)
+                p2.addRelation(d[1]);
+
+            Relationship r = new Relationship(d[0],p1,p2,d[3],d[4],d[5]);
+            relations.put(d[0], r);
+            i++;
+        }
+        //all Relationships are now loaded
+
+        i += 2; //skip the empty row between sections
+
+        //Loading Children into the Relationship children array
+        for(; i<data.size(); i++){
+            String[] d = scrapeData(data.get(i));
+
+            people.get(d[1]).setChildOfR(d[0]);
+            relations.get(d[0]).addChild(people.get(d[1]));
+        }
+        //All children loaded
+
+    }
+
+    //converts a string into a string array and converts any empty values to null through a mapping
+    private static String[] scrapeData(String data){
+        List<String> d = Arrays.asList(data.split(","));
+        Object[] o = d.stream().map(n -> (n.equals(" ") ? null : n)).toArray();
+        return Arrays.copyOf(o, o.length, String[].class);
+    }
+
+    //file reader and loads into a list of strings.
+    private static List<String> readFile(String fileName) throws IOException {
+        Scanner sc = new Scanner(new File(fileName));
+        List<String> lines = new ArrayList<>();
+
+        while(sc.hasNext())
+            lines.add(sc.nextLine());
+
+        sc.close();
+        return lines;
+    }
+
+}
--- src/SwingUI/EntryPage.java
+++ src/SwingUI/EntryPage.java
@@ -80,47 +80,18 @@ public class EntryPage {
      * @noinspection ALL
      */
     private void $$$setupUI$$$() {
-        EntryScreen = new JPanel();
-        EntryScreen.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(7, 1, new Insets(0, 0, 0, 0), -1, -1));
-        EntryScreen.setBackground(new Color(-11688872));
-        EntryScreen.setForeground(new Color(-16777216));
-        EntryScreen.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(new Color(-16777216)), "Welcome to the Geneology Tree App!", TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION, this.$$$getFont$$$("Consolas", Font.BOLD, 18, EntryScreen.getFont()), new Color(-16777216)));
-        promptText = new JTextField();
-        promptText.setBackground(new Color(-11688872));
-        promptText.setEditable(false);
-        promptText.setEnabled(true);
-        Font promptTextFont = this.$$$getFont$$$("Consolas", Font.BOLD, 16, promptText.getFont());
-        if (promptTextFont != null) promptText.setFont(promptTextFont);
-        promptText.setForeground(new Color(-16777216));
-        promptText.setHorizontalAlignment(0);
-        promptText.setText("What would you like to do?");
-        EntryScreen.add(promptText, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        importFileButton = new JButton();
-        importFileButton.setBackground(new Color(-1));
-        importFileButton.setEnabled(true);
-        Font importFileButtonFont = this.$$$getFont$$$("Consolas", Font.PLAIN, 14, importFileButton.getFont());
-        if (importFileButtonFont != null) importFileButton.setFont(importFileButtonFont);
-        importFileButton.setForeground(new Color(-16777216));
-        importFileButton.setText("Create Tree from File");
+        EntryScreen = new javax.swing.JPanel();
+        EntryScreen.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(7, 1, new java.awt.Insets(0, 0, 0, 0), -1, -1));
+        EntryScreen.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(-16777216)), "Welcome to the Geneology Tree App!", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, this.$$$getFont$$$("Consolas", java.awt.Font.BOLD, 18, EntryScreen.getFont()), new java.awt.Color(-16777216)));
+        promptText = new javax.swing.JTextField();
+        EntryScreen.add(promptText, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new java.awt.Dimension(150, -1), null, 0, false));
+        importFileButton = new javax.swing.JButton();
         EntryScreen.add(importFileButton, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        viewExistingButton = new JButton();
-        viewExistingButton.setBackground(new Color(-1));
-        viewExistingButton.setEnabled(true);
-        Font viewExistingButtonFont = this.$$$getFont$$$("Consolas", Font.PLAIN, 14, viewExistingButton.getFont());
-        if (viewExistingButtonFont != null) viewExistingButton.setFont(viewExistingButtonFont);
-        viewExistingButton.setText("View Existing Trees");
+        viewExistingButton = new javax.swing.JButton();
         EntryScreen.add(viewExistingButton, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        button3 = new JButton();
-        button3.setBackground(new Color(-1));
-        Font button3Font = this.$$$getFont$$$("Consolas", Font.PLAIN, 14, button3.getFont());
-        if (button3Font != null) button3.setFont(button3Font);
-        button3.setText("Button");
+        button3 = new javax.swing.JButton();
         EntryScreen.add(button3, new com.intellij.uiDesigner.core.GridConstraints(3, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        button4 = new JButton();
-        button4.setBackground(new Color(-1));
-        Font button4Font = this.$$$getFont$$$("Consolas", Font.PLAIN, 14, button4.getFont());
-        if (button4Font != null) button4.setFont(button4Font);
-        button4.setText("Button");
+        button4 = new javax.swing.JButton();
         EntryScreen.add(button4, new com.intellij.uiDesigner.core.GridConstraints(4, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final com.intellij.uiDesigner.core.Spacer spacer1 = new com.intellij.uiDesigner.core.Spacer();
         EntryScreen.add(spacer1, new com.intellij.uiDesigner.core.GridConstraints(6, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
@@ -131,26 +102,26 @@ public class EntryPage {
     /**
      * @noinspection ALL
      */
-    private Font $$$getFont$$$(String fontName, int style, int size, Font currentFont) {
+    private java.awt.Font $$$getFont$$$(String fontName, int style, int size, java.awt.Font currentFont) {
         if (currentFont == null) return null;
         String resultName;
         if (fontName == null) {
             resultName = currentFont.getName();
         } else {
-            Font testFont = new Font(fontName, Font.PLAIN, 10);
+            java.awt.Font testFont = new java.awt.Font(fontName, java.awt.Font.PLAIN, 10);
             if (testFont.canDisplay('a') && testFont.canDisplay('1')) {
                 resultName = fontName;
             } else {
                 resultName = currentFont.getName();
             }
         }
-        return new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
+        return new java.awt.Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
     }
 
     /**
      * @noinspection ALL
      */
-    public JComponent $$$getRootComponent$$$() {
+    public javax.swing.JComponent $$$getRootComponent$$$() {
         return EntryScreen;
     }
 
